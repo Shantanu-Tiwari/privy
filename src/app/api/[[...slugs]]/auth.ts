e2561e05ba9041a0b6f4ddfec2a5d55879f19265
Shantanu-Tiwari <shantanutiwari240704@gmail.com>
2025-12-11 10:40:13 +0530

    final code

--- src/app/api/[[...slugs]]/auth.ts
+++ src/app/api/[[...slugs]]/auth.ts
@@ -1,29 +1,34 @@
-import Elysia from "elysia";
+import { redis } from "@/lib/redis"
+import Elysia from "elysia"
 
 class AuthError extends Error {
     constructor(message: string) {
-        super(message);
+        super(message)
         this.name = "AuthError"
     }
 }
 
-export const authMiddleware = new Elysia({
-    name: "auth"}).error({AuthError}).onError(({code, set}) => {
+export const authMiddleware = new Elysia({ name: "auth" })
+    .error({ AuthError })
+    .onError(({ code, set }) => {
         if (code === "AuthError") {
             set.status = 401
             return { error: "Unauthorized" }
         }
-}).derive( {as:"scoped"}, async ({query, cookie}) => {
-    const roomId = query.roomId
-    const token = cookie["x-auth-token"].value as string | undefined
-    if (!roomId || !token) {
-        throw new AuthError("Missing roomId or token")
-    }
+    })
+    .derive({ as: "scoped" }, async ({ query, cookie }) => {
+        const roomId = query.roomId
+        const token = cookie["x-auth-token"].value as string | undefined
 
-    const connected = await redis.hget<string[]>(`meta.${roomId}`, "connected")
+        if (!roomId || !token) {
+            throw new AuthError("Missing roomId or token.")
+        }
 
-    if(!connected?.includes(token)) {
-        throw new AuthError("Invalid token")
-    }
-    return {auth: {roomId, token, connected}}
-})
+        const connected = await redis.hget<string[]>(`meta:${roomId}`, "connected")
+
+        if (!connected?.includes(token)) {
+            throw new AuthError("Invalid token")
+        }
+
+        return { auth: { roomId, token, connected } }
+    })
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,11 +2,14 @@
 
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {useState} from "react";
+import {RealtimeProvider} from "@upstash/realtime/client";
 
 export const Providers = ({children} : {children: React.ReactNode} ) => {
     const [queryClient] = useState(() => new QueryClient())
 
-    return <QueryClientProvider client={queryClient}>
-        {children}
-    </QueryClientProvider>
+    return <RealtimeProvider>
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    </RealtimeProvider>
 }
